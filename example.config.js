643ef926de03f1933f72bc17b44df3a5de3ca888
decaai <164585657+decaai@users.noreply.github.com>
2025-01-28 11:44:23 +0700

    Update example.config.js

--- example.config.js
+++ example.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: '', // https://core.telegram.org/bots/faq#how-do-i-create-a-bot
-    apiKey: '', // https://beta.openai.com/account/api-keys
+    token: '7719187128:AAGLV8YYPA3cz8SRLb7bAb-S3_Pw6e8SDIc', // https://core.telegram.org/bots/faq#how-do-i-create-a-bot
+    apiKey: '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', // https://beta.openai.com/account/api-keys
 }
